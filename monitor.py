98f9dd34118ca84c30cc4995b879415616c602ba
antares <32589030+Antares0982@users.noreply.github.com>
2021-02-20 00:12:32 +0800

    v1.0
    不会有v1.1

--- monitor.py
+++ monitor.py
@@ -14,14 +14,15 @@ myid = cfgparser.getint("BOT", "MYID")
 
 if use_proxy:
     proxy_url = cfgparser.get("PROXY", "PROXY_URL")
-    updater = Updater(token=token, request_kwargs={'proxy_url': proxy_url}, use_context=True)
+    updater = Updater(token=token, request_kwargs={
+                      'proxy_url': proxy_url}, use_context=True)
 else:
     updater = Updater(token=token, use_context=True)
 
 with open(filename, 'r', encoding="utf-8") as f:
     txt = f.read()
-    
-if txt!="":
+
+if txt != "":
     updater.bot.send_message(chat_id=myid, text=txt)
     with open(filename, 'w', encoding='utf-8') as f:
         f.write("")
